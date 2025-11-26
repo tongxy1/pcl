f24a93001edf55415a0bfe6e4e87d71122d740b2
tongxy <tongxiaoyan@yahoo.com>
2025-11-26 18:26:37 -0500

    just for save

--- ch1/pca_normal.py
+++ ch1/pca_normal.py
@@ -37,12 +37,12 @@ def PCA(data, correlation=False, sort=True):
 
 def main():
     # 加载原始点云
-    with open('/RobotGrasp/pcl/ch1/Homework I/modelnet40_normal_resampled/'
+    with open('/RobotGrasp/pcl/ch1/modelnet40_normal_resampled/'
               'modelnet40_shape_names.txt') as f:
         a = f.readlines()
     for i in a:
         point_cloud_pynt = PyntCloud.from_file(
-            '/RobotGrasp/pcl/ch1/Homework I/modelnet40_normal_resampled/'
+            '/RobotGrasp/pcl/ch1/modelnet40_normal_resampled/'
             '{}/{}_0001.txt'.format(i.strip(), i.strip()), sep=",",
             names=["x", "y", "z", "nx", "ny", "nz"])
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
@@ -64,21 +64,38 @@ def main():
     frame.rotate(R, center=[0, 0, 0])
     frame.translate(t)
     frame.scale(5.0, center=[0, 0, 0])   # 整体放大
-    o3d.visualization.draw_geometries([point_cloud_o3d, frame]) # 显示PCA主方向
+    o3d.visualization.draw_geometries([point_cloud_o3d, frame]) # 同时显示点云和PCA主方向
     
     # 循环计算每个点的法向量
     pcd_tree = o3d.geometry.KDTreeFlann(point_cloud_o3d)
     normals = []
-    # 作业2
+    # 作业2    
     # 屏蔽开始
-
-    # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数
+    
+    # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数    
+    points = np.asarray(point_cloud_o3d.points)    
+    for i in range(points.shape[0]):        
+        # 对点 i 找 30 个邻居    
+        _, idx, _ = pcd_tree.search_knn_vector_3d(points[i], 30)
+        # 邻域点
+        neighbor_pts = points[idx, :]        
+        # PCA特征值分解
+        eigenvalues, eigenvectors = PCA(neighbor_pts)        
+        normals.append(eigenvectors[:, -1])# 法向量 = 最小特征值对应的特征向量
 
     # 屏蔽结束
+
     normals = np.array(normals, dtype=np.float64)
     # TODO: 此处把法向量存放在了normals中
     point_cloud_o3d.normals = o3d.utility.Vector3dVector(normals)
-    o3d.visualization.draw_geometries([point_cloud_o3d])
+    
+    # 显示带法向量的点云
+    # 统一法线方向（例如朝向原点）
+    point_cloud_o3d.orient_normals_towards_camera_location(camera_location=(0, 0, 0))
+    # 可视化
+    o3d.visualization.draw_geometries([point_cloud_o3d], point_show_normal=True)
+
+    # o3d.visualization.draw_geometries([point_cloud_o3d])
 
 
 if __name__ == '__main__':
